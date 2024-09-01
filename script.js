a37b7a5f3a2b8b0a5f0419363ac758d63c5f0ba2
U~Iriamuzu <daviswill264@gmail.com>
2024-09-01 12:29:52 +0100

    Fourth Commit

--- script.js
+++ script.js
@@ -1,75 +1,112 @@
-const step2 = document.querySelector('.stepTwo')
+const stepOne = document.querySelector('.step1'),
+          stepTwo = document.querySelector('.step2'),
+          stepThree = document.querySelector('.step3'),
+          stepFour = document.querySelector('.step4'),
+          stepFive = document.querySelector('.step5'); 
 
-step2.addEventListener('click', () => {
+let btnOne = document.querySelector('#btnOne'),
+      btnTwo = document.querySelector('#btnTwo'),
+      btnThree = document.querySelector('#btnThree'),
+      btnFour = document.querySelector('#btnFour'),
+
+      prevTwo = document.querySelector('#prevTwo'),
+      prevThree = document.querySelector('#prevThree'),
+      prevFour = document.querySelector('#prevFour'),
+
+      circleOne = document.querySelector('#circleOne'),
+      circleTwo = document.querySelector('#circleTwo'),
+      circleThree = document.querySelector('#circleThree'),
+      circleFour = document.querySelector('#circleFour');
   
+let emailInput = document.querySelector('#email'),
+        emailError = document.querySelector('.emailError'),
+        emailCheck = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        nameInput = document.querySelector('#name'),
+        nameError = document.querySelector('.nameError'),
+        nameCheck = /^[a-zA-Z-\s]+$/,
+        numberInput = document.querySelector("#number"),
+        numberError = document.querySelector(".numberError"),
+        numberCheck = /^[0-9\s]+$/;
+
+btnOne.addEventListener('click', () => {
+  checkName();
+  checkEmail();
+  checkNumber();
+  setTimeout(() => {
+    btnOne.style.backgroundColor = "var(--Marine-blue";
+    Achieve();
+  }, 1000);
+})
+
+btnTwo.addEventListener('click', () => {
+
 })
 
 function checkName () {
-  let nameInput = document.querySelector('#name'),
-        nameError = document.querySelector('.nameError'),
-        nameCheck = /^[a-zA-Z-\s]+$/;
-
   if (nameInput.value == ''){
     nameError.innerHTML = 'This field is required'
+    btnOne.style.backgroundColor = "var(--Light-Marine-blue";
+  } else if (nameCheck.test(nameInput.value) == false) {
+    nameError.innerHTML = "Can't use number or symbols";
+    btnOne.style.backgroundColor = "var(--Light-Marine-blue";
   } else if (nameInput.value !== ''){
     nameError.innerHTML = ''
   } 
-  if (nameCheck.test(nameInput.value) == false) {
-    nameError.innerHTML = "Can't use number or symbols"
-  }
 }
 
-function checkMail() {
-  let nameInput = document.querySelector('#name'),
-        nameError = document.querySelector('.nameError'),
-        emailCheck = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-  if (myMailInput.value.trim() == "") {
-    myNumError.innerHTML = "This field is required";
-  } else if (emailCheck.test(myMailInput.value) == false) {
-    myNumError.innerHTML = "Please enter a valid e-mail";
+function checkEmail() {
+  if (emailInput.value == "") {
+    emailError.innerHTML = "This field is required";
+    btnOne.style.backgroundColor = "var(--Light-Marine-blue";
+  } else if (emailCheck.test(emailInput.value) == false) {
+    emailError.innerHTML = "Please enter a valid e-mail";
+    btnOne.style.backgroundColor = "var(--Light-Marine-blue";
   } else {
-    myNumError.innerHTML = "";
+    emailError.innerHTML = "";
   }
 }
 
 function checkNumber() {
-  let myNumInput = document.querySelector("#number"),
-        myNumError = document.querySelector("errorNum"),
-        myNumRegex = /^[0-9\s]+$/;
-
-  if (myNumInput.value.trim() == "") {
-    myNumError.innerHTML = "This field is required";
-  } else if (myNumRegex.test(myNumInput.value) == false) {
-    myNumError.innerHTML = "Must contain number";
-  } else if (myNumInput.value.trim().length !== 10) {
-    myNumError.innerHTML = "Invalid Phone Number";
+  if (numberInput.value == "") {
+    numberError.innerHTML = "This field is required";
+    btnOne.style.backgroundColor = "var(--Light-Marine-blue";
+  } else if (numberCheck.test(numberInput.value) == false) {
+    numberError.innerHTML = "Must contain number";
+    btnOne.style.backgroundColor = "var(--Light-Marine-blue";
+  } else if (numberInput.value.length !== 11) {
+    numberError.innerHTML = "Invalid Phone Number";
+    btnOne.style.backgroundColor = "var(--Light-Marine-blue";
   } else {
-    myNumError.innerHTML = "";
+    numberError.innerHTML = "";
   }
 }
 
 function Achieve() {
-  let myNumInput = document.getElementById("infoNumber");
-  let myNameInput = document.getElementById("infoName");
-  let myMailInput = document.getElementById("infoMail");
-  let myNameRegex = /^[a-zA-Z-\s]+$/;
-  let myNumRegex = /^[0-9\s]+$/;
-  let emailCheck = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if ( nameInput.value.length !== 0 && nameCheck.test(nameInput.value) === true && emailInput.value.length !== 0 && emailCheck.test(emailInput.value) === true && numberInput.value.length === 11 && numberCheck.test(numberInput.value) == true) {
+    stepOne.style.display = 'none';
+    stepTwo.style.display = 'grid';
 
-  if (
-    myNumInput.value.trim().length === 10 &&
-    myNumRegex.test(myNumInput.value) == true &&
-    myMailInput.value.trim().length !== 0 &&
-    emailCheck.test(myMailInput.value) === true &&
-    myNameInput.value.trim().length !== 0 &&
-    myNameRegex.test(myNameInput.value) === true
-  ) {
-    stepInfo.style.display = "none";
-    stepPlan.style.display = "flex";
-    circle1.style.color = "white";
-    circle1.style.backgroundColor = "rgba(255, 255, 255, 0)";
-    circle2.style.backgroundColor = "rgb(255, 255, 255)";
-    circle2.style.color = "black";
+    circleOne.style.color = "var(--Marine-blue)";
+    circleOne.style.backgroundColor = "var(--Light-blue)";
   }
 }
+
+
+prevTwo.addEventListener('click', () => {
+  stepOne.style.display = 'grid';
+  stepTwo.style.display = 'none';
+  circleOne.style.backgroundColor = 'transparent'
+  circleOne.style.color = 'var(--White)'
+})
+prevThree.addEventListener('click', () => {
+  stepTwo.style.display = 'grid';
+  stepThree.style.display = 'none';
+  circleTwo.style.backgroundColor = 'transparent'
+  circleTwo.style.color = 'var(--White)'
+})
+prevFour.addEventListener('click', () => {
+  stepThree.style.display = 'grid';
+  stepFour.style.display = 'none';
+  circleThree.style.backgroundColor = 'transparent'
+  circleThree.style.color = 'var(--White)'
+})
